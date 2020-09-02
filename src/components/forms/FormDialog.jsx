8b9c2b30c7df0ea7cd11a60b6920a299d4fd9f4d
Masayuki <soy2.san@gmail.com>
2020-09-02 18:14:40 +0900

    first

---
+++ src/components/forms/FormDialog.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useCallback } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextInput from './TextInput'
+
+
+const FormDialog = (props) => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [description, setDescription] = useState("");
+
+    const inputName = useCallback((event) => {
+        setName(event.target.value)
+    }, [setName]);
+
+    const inputEmail = useCallback((event) => {
+        setEmail(event.target.value)
+    }, [setEmail]);
+
+    const inputDescription = useCallback((event) => {
+        this.setState({ description: event.target.value })
+    }, [setDescription]);
+
+    const submitForm = () => {
+        const name = setName;
+        const email = setEmail;
+        const description = setDescription;
+
+        const payload = {
+            text: 'お問い合わせがありました\n' +
+                'お名前:' + name + '\n' +
+                'Email:' + email + '\n' +
+                'お問い合わせ内容: \n' + description
+        }
+
+        const url = "https://hooks.slack.com/services/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";
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(payload)
+        }).then(() => {
+            alert('送信が完了しました。追ってご連絡いたします！')
+            setName("")
+            setEmail("")
+            setDescription("")
+            return props.handleClose()
+        })
+    };
+
+    return (
+        <Dialog
+            open={props.open}
+            onClose={props.handleClose}
+        >
+            <DialogTitle>
+                お問い合わせフォーム
+            </DialogTitle>
+            <DialogContent>
+                <TextInput
+                    label={"お名前（必須）"} multiline={false} rows={1}
+                    value={name} type={'text'} onChange={inputName}
+                />
+                <TextInput
+                    label={"メールアドレス（必須）"} multiline={false} rows={1}
+                    value={email} type={'email'} onChange={inputEmail}
+                />
+                <TextInput
+                    label={"お問い合わせ内容（必須）"} multiline={true} rows={5}
+                    value={description} type={'text'} onChange={inputDescription}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={props.handleClose} color="primary">
+                    キャンセル
+                    </Button>
+                <Button onClick={submitForm} color="primary" autoFocus>
+                    送信する
+                    </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default FormDialog;
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAqEGy9korZN9cDu_hGAXW9IxKTSETxfos",
+    authDomain: "chatbot-a87e0.firebaseapp.com",
+    databaseURL: "https://chatbot-a87e0.firebaseio.com",
+    projectId: "chatbot-a87e0",
+    storageBucket: "chatbot-a87e0.appspot.com",
+    messagingSenderId: "143337464981",
+    appId: "1:143337464981:web:c0156988022ea6282c679c",
+    measurementId: "G-WE3NNPM5R2"
+};
+
+export default firebaseConfig;
